84e96d05fcdd6759b783ad5f59bd8622ff49bec2
vkhristenko <vdkhristenko1991@gmail.com>
2018-12-09 13:50:07 +0100

    checked strings, primitive data types now

--- intro/char_array.c
+++ intro/char_array.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char str_a[20];
-    str_a[0] = 'H';
-    str_a[1] = 'e';
-    str_a[2] = 'l';
-    str_a[3] = 'l';
-    str_a[4] = 'o';
-    str_a[5] = ',';
-    str_a[6] = ' ';
-    str_a[7] = 'w';
-    str_a[8] = 'o';
-    str_a[9] = 'r';
-    str_a[10] = 'l';
-    str_a[11] = 'd';
-    str_a[12] = '!';
-    str_a[13] = '\n';
-    str_a[14] = 0;
 
+    strcpy(str_a, "Hello, world! geruguegte hgw \n");
     printf("%s", str_a);
 
     return 0;
