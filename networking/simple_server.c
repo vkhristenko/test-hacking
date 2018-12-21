b50461c8dd433352fa3c9ee69b5a84bdf63f7fd3
vkhristenko <vdkhristenko1991@gmail.com>
2018-12-21 23:11:05 +0100

    adding common networking funcs + ip lookup example via dns

--- networking/simple_server.c
+++ networking/simple_server.c
@@ -10,7 +10,7 @@
 
 int main(void) {
     int sockfd, new_sockfd; // listen on sockfd, new conn on new_sockfd
-    struct sockaddr_in host_addr, client_add; // my address information
+    struct sockaddr_in host_addr, client_addr; // my address information
     socklen_t sin_size;
     int recv_length=1, yes=1;
     char buffer[1024];
@@ -20,4 +20,34 @@ int main(void) {
 
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
         fatal("setting socket option SO_REUSEADDR");
+
+    host_addr.sin_family = AF_INET; // host byte order
+    host_addr.sin_port = htons(PORT); // short, network byte order
+    host_addr.sin_addr.s_addr = 0; // automatically fill with my ip
+    memset(&(host_addr.sin_zero), '\0', 8); // zero the rest of the struct
+
+    if (bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr)) == -1)
+        fatal("binding to socket");
+
+    if (listen(sockfd, 5) == -1)
+        fatal("listening on socket");
+
+    while (1) {
+        sin_size = sizeof(struct sockaddr_in);
+        new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
+        if (new_sockfd == -1)
+            fatal("accepting connection");
+        printf("server: got connection from %s port %d\n",
+            inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        send(new_sockfd, "hello, world!\n", 13, 0);
+        recv_length = recv(new_sockfd, &buffer, 1024, 0);
+        while (recv_length > 0) {
+            printf("RECV: %d bytes\n", recv_length);
+            dump(buffer, recv_length);
+            recv_length = recv(new_sockfd, &buffer, 1024, 0);
+        }
+        close(new_sockfd);
+    }
+
+    return 0;
 }
