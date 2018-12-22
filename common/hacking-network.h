bd2d3fbf6bf17c86a5b89478a956b24b536ba74c
vkhristenko <vdkhristenko1991@gmail.com>
2018-12-23 00:13:20 +0300

    adding ethernet, ip, tcp header struct declarations

--- common/hacking-network.h
+++ common/hacking-network.h
@@ -44,3 +44,44 @@ int recv_line(int sockfd, unsigned int *dest_buffer) {
 
     return 0;
 }
+
+#define ETHER_ADDR_LEN 6
+#define ETHER_HDR_LEN 14
+
+struct ether_hdr {
+    unsigned char ether_dest_addr[ETHER_ADDR_LEN];
+    unsigned char ether_src_addr[ETHER_ADDR_LEN];
+    unsigned short ether_type;
+};
+
+struct ip_hdr {
+    unsigned char ip_version_and_header_length; // version and header length 
+    unsigned char ip_tos; // type of service
+    unsigned short ip_len; // total length
+    unsigned short ip_id; // identification number
+    unsigned short ip_frag_offset; // fragment offset and flags
+    unsigned char ip_ttl; // time to live
+    unsigned char ip_type; // protocol type
+    unsigned short ip_checksum; // checksum
+    unsigned int ip_src_addr; // source ip address
+    unsigned int ip_dest_addr; // destination ip address
+};
+
+struct tcp_hdr {
+    unsigned short tcp_src_port; // source tcp port
+    unsigned short tcp_dest_port; // destination tcp port
+    unsigned int tcp_seq; // tcp sequence number
+    unsigned int tcp_ack; // tcp acknowledgment number
+    unsigned char reserved : 4; // 4 bits from the 6 bits of reserved space
+    unsigned char tcp_offset : 4; // tcp data offset for little-endian host
+    unsigned char tcp_flags; // tcp flags (and 2 bits from reserved space)
+#define TCP_FIN 0x01
+#define TCP_SYN 0x02
+#define TCP_RST 0x04
+#define TCP_PUSH 0x08
+#define TCP_ACK 0x10
+#define TCP_URG 0x20
+    unsigned short tcp_window; // tcp window size
+    unsigned short tcp_checksum; // tcp checksum
+    unsigned short tcp_urgent; // tcp urgent pointer
+};
