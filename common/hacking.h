8b999f045a79253a016034bf5d8590ae10b27fb6
vkhristenko <vdkhristenko1991@gmail.com>
2019-01-26 00:21:38 +0100

    redoing a tcp/ip server example, with a telnet client

--- common/hacking.h
+++ common/hacking.h
@@ -20,24 +20,24 @@ void *ec_malloc(unsigned int size) {
 }
 
 // dumps raw memory in hex byte and printable split format
-void dump(unsigned char const *data_buffer, unsigned int const length) {
+void dump(const unsigned char *data_buffer, const unsigned int length) {
     unsigned char byte;
     unsigned int i,j;
     for (i=0; i<length; i++) {
         byte = data_buffer[i];
-        printf("%02x", data_buffer[i]);
+        printf("%02x", data_buffer[i]); // display byte in hex
         if (((i%16) == 15) || (i==length-1)) {
             for (j=0; j<15-(i%16); j++)
                 printf("  ");
             printf("| ");
-            for (j=(i-(i%16)); j<=i; j++) { // diplsya printable bytes from line.
+            for (j=(i-(i%16)); j<=i; j++) { // display printable bytes from line
                 byte = data_buffer[j];
-                if ((byte > 31) && (byte < 127)) // outside printable char range
+                if ((byte > 31) && (byte<127)) // outside printable char range
                     printf("%c", byte);
                 else
                     printf(".");
             }
-            printf("\n");
-        }
-    }
+            printf("\n"); // end of hte dump line (each line is 16 bytes)
+        } // end if 
+    } // end for
 }
